7c43016c3d7cd4d19e091ac37638c67810def805
Bastien Nocera <hadess@hadess.net>
2007-11-19 15:11:04 +0000

    Patch from Fred Hsueh <fred.hsueh@gmail.com> to fix loading the Pidgin
    2007-11-19  Bastien Nocera  <hadess@hadess.net>
    
    	* pidgin_plugin/nautilus-sendto-plugin.c: (save_online_buddies):
    	Patch from Fred Hsueh <fred.hsueh@gmail.com> to fix loading the
    	Pidgin plugin (Closes: #466427)
    
    
    svn path=/trunk/; revision=259

--- pidgin_plugin/nautilus-sendto-plugin.c
+++ pidgin_plugin/nautilus-sendto-plugin.c
@@ -123,10 +123,10 @@ save_online_buddies (){
 	    fclose (fd);
 	    g_string_free (buddies_str, TRUE);
 	    buddies_str = str;
-	    gaim_debug_info ("nautilus", "save blist online\n");
+	    purple_debug_info ("nautilus", "save blist online\n");
 	}else{
 	    g_string_free (str, TRUE);
-	    gaim_debug_info ("nautilus", "don't save blist online. No change\n");
+	    purple_debug_info ("nautilus", "don't save blist online. No change\n");
 	}
 	g_free (fd_name);
     }else{
