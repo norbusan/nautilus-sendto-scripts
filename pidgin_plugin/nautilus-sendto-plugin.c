ece24783562fd26e5361f39874f5b714218367f2
Bastien Nocera <hadess@hadess.net>
2008-05-27 08:51:58 +0000

    Revert making the plugin invisible, it apparently makes it impossible to
    2008-05-27  Bastien Nocera  <hadess@hadess.net>
    
    	* pidgin_plugin/nautilus-sendto-plugin.c: Revert making the plugin
    	invisible, it apparently makes it impossible to disable/enable the 
    	plugin, even though it should be enabled by default...
    	https://bugzilla.redhat.com/show_bug.cgi?id=242289#c7
    
    
    svn path=/trunk/; revision=324

--- pidgin_plugin/nautilus-sendto-plugin.c
+++ pidgin_plugin/nautilus-sendto-plugin.c
@@ -307,7 +307,7 @@ static PurplePluginInfo info = {
     PURPLE_MINOR_VERSION,
     PURPLE_PLUGIN_STANDARD,		/* type */
     PIDGIN_PLUGIN_TYPE,			/* ui requirement */
-    PURPLE_PLUGIN_FLAG_INVISIBLE,	/* flags */
+    PURPLE_PRIORITY_DEFAULT,		/* flags */
     NULL,				/* dependencies */
     PURPLE_PRIORITY_DEFAULT,		/* priority */
 
